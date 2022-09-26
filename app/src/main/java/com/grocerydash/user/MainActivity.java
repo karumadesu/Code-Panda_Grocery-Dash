8a47fa0ff5c60d10bae4721aada63996300b140e
Karl Matthew Parafina <56289633+karumadesu@users.noreply.github.com>
2022-09-26 17:31:23 +0800

    Update

--- app/src/main/java/com/grocerydash/user/MainActivity.java
+++ app/src/main/java/com/grocerydash/user/MainActivity.java
@@ -1,13 +1,79 @@
 package com.grocerydash.user;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity{
+
+    private ImageButton imageButtonMenu, imageButtonCart;
+    private RecyclerView recyclerViewPopularProducts;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    ArrayList<PopularProducts> PopularProducts = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewPopularProducts = findViewById(R.id.recyclerview_popularProducts);
+        recyclerViewPopularProducts.setLayoutManager(new LinearLayoutManager(this));
+
+        imageButtonMenu = findViewById(R.id.image_button_menu);
+        imageButtonMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        imageButtonCart = findViewById(R.id.image_button_cart);
+        imageButtonCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("TAG", "Error adding document", e);
+                    }
+                });
+
+    }
+
+    private void setUpPopularProductModels(){
+
     }
 }
